fb11f11892bfea765b4dc30cfa28c738a1fc52b6
Marlon Lorram <eiskyenet@gmail.com>
2024-03-23 14:45:31 -0300

    feat(git): add initial git configuration

---
+++ git/git.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# The directory of the dotfiles
+dotfiles_dir="~/dotfiles/git"
+
+# Use a for loop to handle the files
+for file in $(find $dotfiles_dir -name '.git*'); do
+    ln -fs $file ~/
+done
